11d3812a27f5540574f66cf3845f9a09e90adedb
InspectorDave <55307710+InspectorDave@users.noreply.github.com>
2019-11-19 03:57:52 -0300

    Add files via upload

--- fft.h
+++ fft.h
@@ -5,10 +5,11 @@
 #include <cmath>
 #include "complejo.h"
 #include "vector_t.h"
+#include "dft.h"
 
 vector_t fft(vector_t & vector_in);
 vector_t _fft (vector_t & vector_in);
 vector_t ifft(vector_t & vector_in);
-vector_t fill0till_exp2 (vector_t & vector_in, int & nappends);
+vector_t fill0till_exp2 (vector_t & vector_in);
 
 #endif
