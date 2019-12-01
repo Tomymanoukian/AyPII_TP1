c437fcc5927f49c5d9803f4b1e24f3e9c2f7a99d
InspectorDave <55307710+InspectorDave@users.noreply.github.com>
2019-12-01 06:11:31 -0300

    Utilizo Swap en vez de Append para mayor rapidez

--- fft.cpp
+++ fft.cpp
@@ -9,29 +9,31 @@ vector_t fft (vector_t & vector) {
 
 void _fft (vector_t & vector) {
 
-    int i, largo = vector.leng();
+    int i, j, largo = vector.leng();
     complejo aux, wn;
 
     if (vector.leng() >= 2) {
 
         vector_t vector_par(largo / 2), vector_impar(largo / 2); //crear con tamaño determinado
 
-        for (i = 0; i < largo; i += 2) { //Crea el vector con los indices par
+        for (i = 0, j = 0; i < largo; i += 2, j++) { //Crea el vector con los indices par
 
             aux = vector[i];
-            vector_par.append(aux);
+            //vector_par.append(aux);
+            vector_par.swap(aux, j);
         }
 
-        for (i = 1; i < largo; i += 2) { //Crea el vector con los indices impar
+        for (i = 1, j = 0; i < largo; i += 2, j++) { //Crea el vector con los indices impar
             
             aux = vector[i];
-            vector_impar.append(aux);
+            //vector_impar.append(aux);
+            vector_impar.swap(aux,j);
         }
 
         _fft (vector_par);
         _fft (vector_impar);
 
-        vector.clean();
+        //vector.clean();
 
         for (i = 0; i < largo/2; i++) {
 
@@ -40,7 +42,8 @@ void _fft (vector_t & vector) {
 
             aux = vector_par[i] + vector_impar[i] * wn;
 
-            vector.append(aux);
+            //vector.append(aux);
+            vector.swap(aux, i);
         }
 
         for(; i<largo; i++) {
@@ -50,7 +53,8 @@ void _fft (vector_t & vector) {
 
             aux = vector_par[i - largo/2] + vector_impar[i - largo/2] * wn;
 
-            vector.append(aux);
+            //vector.append(aux);
+            vector.swap(aux, i);
         }
     }
 }
